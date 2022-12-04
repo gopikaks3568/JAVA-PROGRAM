34b641145699aafaf956e706ea96d0a1a94e0827
Gopika K S <101276417+gopikaks3568@users.noreply.github.com>
2022-12-04 17:02:25 +0530

    java program basics

---
+++ hellojava.java
@@ -0,0 +1,20 @@
+import java.text.NumberFormat;
+
+import javax.annotation.processing.SupportedOptions;
+import javax.naming.ldap.SortKey;
+import javax.swing.JSpinner.NumberEditor;
+import javax.swing.text.html.HTMLDocument.RunElement;
+
+public class hellojava {
+   public static void main(String arg[]){
+    NumberFormat currency= NumberFormat.getPercentInstance();
+    String result= currency.format(125652354.5213);
+    System.out.println(result);
+    
+
+     
+
+
+   }
+}
+    
